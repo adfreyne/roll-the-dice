10cb7c777577b16c33a86e54ba775cf9c645192f
Adriaan De Freyne <adriaandefreyne@gmail.com>
2018-07-12 13:51:54 +0200

    add proptypes

--- src/app.js
+++ src/app.js
@@ -2,25 +2,29 @@ import React, { PureComponent } from 'react';
 import { roll } from './store';
 import { connect } from 'react-redux';
 import Dice from './Dice';
-
+import NumberOfDiceForm from './NumberOfDiceForm';
+import PropTypes from 'prop-types';
 class App extends PureComponent {
     render () {
         const { dispatch } = this.props;
         return (
             <div>
+                <NumberOfDiceForm />
                 <button onClick={() => {
                     dispatch({ type: 'ROLL', payload: roll() });
                 }}>
                     Roll'em
                 </button>
-
                 <Dice />
             </div >
         );
     }
 }
 const mapStateToProps = (state) => ({
-    dices: state.dices,
     dice: state.dice
 });
+App.propTypes = {
+    dice: PropTypes.array,
+    dispatch: PropTypes.func
+};
 export default connect(mapStateToProps)(App);
